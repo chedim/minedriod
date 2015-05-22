e52bd77e5b0bb4592ddfa246f68541e8291fc624
Dmitrii Chechetkin <job@chedim.com>
2015-05-22 16:45:32 -0400

    AutoGenerated R class

---
+++ gen/com/onkiup/minedroid/R.java
@@ -0,0 +1,14 @@
+package com.onkiup.minedroid;
+
+import com.onkiup.minedroid.gui.resources.*;
+import com.onkiup.minedroid.gui.MineDroid;
+public final class R {
+	final static String MODID = "minedroid";
+	public final static class id {
+	}
+
+	public final static class ninepatch {
+		public final static ResourceLink panel = new ResourceLink(MODID, "ninepatches", "panel", new EnvParams[] { new EnvParams(null, null, null, null) });
+	}
+
+}
